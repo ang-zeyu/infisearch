fa0e9fbfdef73da27c15826455e6ecf936db857c
Ze Yu <angzeyu@gmail.com>
2021-02-18 13:06:41 +0800

    Enable HMR

--- packages/search/webpack.dev.js
+++ packages/search/webpack.dev.js
@@ -8,6 +8,9 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+    hot: true,
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
