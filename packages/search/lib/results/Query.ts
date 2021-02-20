6bfcf1515d7282bea930b4f2f9b6866d8875648a
Ze Yu <angzeyu@gmail.com>
2021-02-20 19:47:25 +0800

    Scale impact order retrieval amount by idf

--- packages/search/lib/results/Query.ts
+++ packages/search/lib/results/Query.ts
@@ -1,6 +1,5 @@
 import Result from './Result';
 import Storage from './Storage';
-import PostingsListManager from '../PostingsList/PostingsListManager';
 import FieldInfo from './FieldInfo';
 import PostingsList from '../PostingsList/PostingsList';
 import Dictionary from '../Dictionary/Dictionary';
@@ -28,42 +27,44 @@ class Query {
   async retrieve(n: number): Promise<Result[]> {
     const N = this.docLengths[0][0]; // first line is number of documents
 
-    const docScores: { [docId:number]: { [fieldId: number]: number } } = {};
+    const docScores: { [docId:number]: number } = {};
 
-    const r = n;
+    const meanIdf = this.queriedTerms
+      .reduce((acc, term) => acc + Math.log10(N / this.dictionary.termInfo[term].docFreq), 0)
+      / this.queriedTerms.length;
 
     // Tf-idf computation
     this.queriedTerms.forEach((term) => {
       const postingsList = this.postingsLists[term];
-      const nextRDocs = postingsList.getDocs(r);
       const idf = Math.log10(N / this.dictionary.termInfo[term].docFreq);
 
+      const r = Math.floor((idf / meanIdf) > 1 ? (idf / meanIdf) * n : n) * 2;
+      const nextRDocs = postingsList.getDocs(r);
+
       nextRDocs.forEach((fields, docId) => {
-        docScores[docId] = docScores[docId] ?? {};
+        let wfTD = 0;
 
         Object.entries(fields).forEach(([fieldId, positions]) => {
+          const fieldIdInt = Number(fieldId);
+          const fieldWeight = this.fieldInfo[fieldIdInt].weight;
+          const fieldLen = this.docLengths[docId][fieldIdInt - 1];
+
           const termFreq = positions.length;
           const wtd = 1 + Math.log10(termFreq);
-          docScores[docId][fieldId] = (docScores[docId][fieldId] ?? 0) + wtd * idf;
+
+          // with normalization and weighted zone scoring
+          wfTD += ((wtd * idf) / fieldLen) * fieldWeight;
         });
+
+        docScores[docId] = (docScores[docId] ?? 0) + wfTD;
       });
     });
 
     const resultHeap: Heap<Result> = new Heap((r1: Result, r2: Result) => r2.score - r1.score);
 
-    // Normalization, weighted zone scoring
-    Object.entries(docScores).forEach(([docId, fieldScores]) => {
+    Object.entries(docScores).forEach(([docId, score]) => {
       const docIdInt = Number(docId);
-      let finalDocScore = 0;
-
-      Object.entries(fieldScores).forEach(([fieldId, fieldScore]) => {
-        const fieldIdInt = Number(fieldId);
-        const fieldWeight = this.fieldInfo[fieldIdInt].weight;
-        const fieldLen = this.docLengths[docIdInt][fieldIdInt - 1];
-        finalDocScore += ((fieldScore / fieldLen) * fieldWeight);
-      });
-
-      resultHeap.push(new Result(docIdInt, finalDocScore));
+      resultHeap.push(new Result(docIdInt, score));
     });
 
     const minAmtResults = Math.min(n, resultHeap.size());
