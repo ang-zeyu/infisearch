6e0a9d3716dd21f47f96dc0f1f6a6fc5ad743d7f
Ze Yu <angzeyu@gmail.com>
2021-02-21 02:23:23 +0800

    Remove non word boundary characters

--- packages/indexer/lib/tokenizers/English.js
+++ packages/indexer/lib/tokenizers/English.js
@@ -2,13 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const flatMap = require('lodash/flatMap');
 const PUNCTUATION_FILTER = new RegExp('[\\[\\](){}&|\'"`<>#:;~_^=\\-‑+*/‘’“”，。《》…—‐•?!,.]', 'g');
+const BOUNDARY_FILTER = new RegExp('(^\\W)|(\\W$)');
 const WHITESPACE_SPLITTER = new RegExp('\\s+', 'g');
 const SENTENCE_SPLITTER = new RegExp('[.?!](?=\\s)', 'g');
 class English {
     // eslint-disable-next-line class-methods-use-this
     tokenize(text) {
         return flatMap(text.toLowerCase().split(SENTENCE_SPLITTER), (sent) => sent.split(WHITESPACE_SPLITTER)
-            .map((term) => term.replace(PUNCTUATION_FILTER, ''))
+            .map((term) => term.replace(PUNCTUATION_FILTER, '').replace(BOUNDARY_FILTER, ''))
             .filter((term) => term.length !== 0 && term.length <= 255));
     }
 }
--- packages/indexer/lib/tokenizers/English.ts
+++ packages/indexer/lib/tokenizers/English.ts
@@ -3,6 +3,7 @@ import Tokenizer from './Tokenizer';
 const flatMap = require('lodash/flatMap');
 
 const PUNCTUATION_FILTER = new RegExp('[\\[\\](){}&|\'"`<>#:;~_^=\\-‑+*/‘’“”，。《》…—‐•?!,.]', 'g');
+const BOUNDARY_FILTER = new RegExp('(^\\W)|(\\W$)');
 
 const WHITESPACE_SPLITTER = new RegExp('\\s+', 'g');
 const SENTENCE_SPLITTER = new RegExp('[.?!](?=\\s)', 'g');
@@ -11,7 +12,7 @@ class English implements Tokenizer {
   // eslint-disable-next-line class-methods-use-this
   tokenize(text: string): string[] {
     return flatMap(text.toLowerCase().split(SENTENCE_SPLITTER), (sent) => sent.split(WHITESPACE_SPLITTER)
-      .map((term) => term.replace(PUNCTUATION_FILTER, ''))
+      .map((term) => term.replace(PUNCTUATION_FILTER, '').replace(BOUNDARY_FILTER, ''))
       .filter((term) => term.length !== 0 && term.length <= 255));
   }
 }
