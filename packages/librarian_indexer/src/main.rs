09cc04d8d62564c46183b1768c191466079e0aba
Ze Yu <angzeyu@gmail.com>
2021-06-05 19:25:21 +0800

    More precise elapsed time

--- packages/librarian_indexer/src/main.rs
+++ packages/librarian_indexer/src/main.rs
@@ -280,6 +280,6 @@ fn main() {
 }
 
 fn print_time_elapsed(instant: Instant, extra_message: &str) {
-    let elapsed = instant.elapsed().as_secs();
-    println!("{} {} mins {} seconds elapsed.", extra_message, elapsed / 60, elapsed % 60);
+    let elapsed = instant.elapsed().as_secs_f64();
+    println!("{} {} mins {} seconds elapsed.", extra_message, (elapsed as u32) / 60, elapsed % 60.0);
 }
