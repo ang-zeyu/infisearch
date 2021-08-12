4b99194807dfb10844c729036aa205d4a78f6c44
Ze Yu <angzeyu@gmail.com>
2021-08-12 16:01:46 +0800

    Get some more configurations ready for publishing

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
   mode: 'production',
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'packages/search-ui/dist'),
   },
   module: {
     rules: [
