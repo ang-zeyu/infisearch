7eba2f270e319d93cbbccde08c963831258bf64e
Ze Yu <angzeyu@gmail.com>
2021-09-04 18:22:06 +0800

    Bitmap tests

--- packages/morsels_common/src/bitmap.rs
+++ packages/morsels_common/src/bitmap.rs
@@ -4,3 +4,19 @@ pub fn check(vec: &[u8], at: usize) -> bool {
     let bit_number = 1_u8 << (at % 8) as u8;
     (vec[byte_number] & bit_number) != 0
 }
+
+#[cfg(test)]
+mod test {
+    use super::check;
+
+    #[test]
+    fn test_bitmap() {
+        assert!(!check(&[0], 0));
+        assert!(check(&[1], 0));
+        assert!(check(&[129], 0));
+        assert!(check(&[129], 7));
+        assert!(check(&[0, 129], 8));
+        assert!(!check(&[0, 129], 9));
+        assert!(check(&[0, 129], 15));
+    }
+}
