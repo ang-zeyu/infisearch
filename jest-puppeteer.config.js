1b0b37cc077048361c005e5eff75ed45f676c696
Ze Yu <angzeyu@gmail.com>
2021-12-12 00:59:44 +0800

    Try npm-run-all instead of jest-dev-server

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -5,14 +5,4 @@ module.exports = {
     product: 'chrome',
   },
   browserContext: 'incognito',
-  server: [
-    {
-      command: 'rimraf ./e2e/output/* && npm run e2eServer',
-      port: 3000,
-    },
-    {
-      command: 'npm run dev',
-      port: 8080,
-    },
-  ],
 };
