ang-zeyu/infisearch

commit 253006f8d5ac4f0320d0de73d3d1c291a3e39557

Flatten search DocInfo to 1-d arr

packages/morsels_search/src/docinfo.rs
@@ -1,9 +1,10 @@
 use morsels_common::BitmapDocinfoDicttableReader;
 
 pub struct DocInfo {
-    pub doc_length_factors: Vec<Vec<f64>>,
+    pub doc_length_factors: Vec<f64>,
     pub doc_length_factors_len: u32,
     pub num_docs: u32,
+    pub num_fields: usize,
 }
 
 impl DocInfo {
@@ -14,21 +15,24 @@ impl DocInfo {
         let mut avg_doc_lengths: Vec<f64> = Vec::new();
         docinfo_rdr.read_docinfo_inital_metadata(&mut num_docs, &mut doc_id_counter, &mut avg_doc_lengths, num_fields);
 
-        let mut doc_length_factors: Vec<Vec<f64>> = Vec::new();
+        let mut doc_length_factors: Vec<f64> = Vec::with_capacity(num_fields * doc_id_counter as usize);
 
         let mut doc_id = 0;
         while doc_id < doc_id_counter {
             doc_id += 1;
 
-            let mut doc_field_lengths: Vec<f64> = Vec::with_capacity(num_fields);
             for avg_doc_length in avg_doc_lengths.iter() {
                 let field_length = docinfo_rdr.read_field_length() as f64;
-                doc_field_lengths.push(field_length / *avg_doc_length);
+                doc_length_factors.push(field_length / *avg_doc_length);
             }
-            doc_length_factors.push(doc_field_lengths);
         }
 
         let doc_length_factors_len = doc_length_factors.len() as u32;
-        DocInfo { doc_length_factors, doc_length_factors_len, num_docs }
+        DocInfo { doc_length_factors, doc_length_factors_len, num_docs, num_fields }
+    }
+
+    #[inline(always)]
+    pub fn get_doc_length_factor(&self, doc_id: usize, field_id: usize) -> f32 {
+        self.doc_length_factors[(doc_id * self.num_fields) + field_id] as f32
     }
 }

packages/morsels_search/src/searcher/query.rs
@@ -356,8 +356,7 @@ impl Searcher {
         for (field_id, field) in td.fields.iter().enumerate() {
             if field.field_tf > 0.0 {
                 let field_info = self.searcher_config.field_infos.get(field_id).unwrap();
-                let field_len_factor =
-                    self.doc_info.doc_length_factors[doc_id as usize][field_id as usize] as f32;
+                let field_len_factor = self.doc_info.get_doc_length_factor(doc_id as usize, field_id as usize);
 
                 doc_term_score += ((field.field_tf * (field_info.k + 1.0))
                     / (field.field_tf

commit c9b668f7667fe4c2981161ef6bbc0b661bba7e31

Fix not query regression
From 253006f

packages/morsels_search/src/docinfo.rs
@@ -27,8 +27,12 @@ impl DocInfo {
             }
         }
 
-        let doc_length_factors_len = doc_length_factors.len() as u32;
-        DocInfo { doc_length_factors, doc_length_factors_len, num_docs, num_fields }
+        DocInfo {
+            doc_length_factors,
+            doc_length_factors_len: doc_id_counter,
+            num_docs,
+            num_fields,
+        }
     }
 
     #[inline(always)]

e2e/e2e.test.js
@@ -141,6 +141,14 @@ const testSuite = async (configFile) => {
   await waitNoResults();
 
   await clearInput();
+  await typeText('npm AND NOT run AND NOT packages');
+  await waitNoResults();
+
+  await clearInput();
+  await typeText('npm AND run AND setup');
+  await assertSingle('npm run setup');
+
+  await clearInput();
   await typeText('body:"Once you have you test files"');
   await assertSingle('once you have you test files');
 