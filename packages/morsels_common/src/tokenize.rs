ang-zeyu/infisearch

commit c9392c651e497fb671b952ad165f7f07c8c6c6dc

Move spelling correction to tokenizer

packages/morsels_common/src/tokenize.rs
@@ -1,6 +1,6 @@
-use smartstring::alias::String as SmartString;
 use std::borrow::Cow;
-use std::collections::BTreeMap;
+
+use crate::dictionary::Dictionary;
 
 #[derive(Clone)]
 #[cfg_attr(test, derive(Debug))]
@@ -30,28 +30,18 @@ pub trait IndexerTokenizer {
 }
 
 pub trait SearchTokenizer {
-    fn search_tokenize(&self, text: String) -> SearchTokenizeResult;
+    fn search_tokenize(&self, text: String, dict: &Dictionary) -> SearchTokenizeResult;
 
     fn is_stop_word(&self, term: &str) -> bool;
-
-    // If true, simply return None / An empty vec for the below two methods
-    fn use_default_fault_tolerance(&self) -> bool;
-
-    fn get_best_corrected_term(
-        &self,
-        term: &str,
-        dictionary: &BTreeMap<SmartString, &'static TermInfo>,
-    ) -> Option<String>;
-
-    fn get_prefix_terms(
-        &self,
-        number_of_expanded_terms: usize,
-        term: &str,
-        dictionary: &BTreeMap<SmartString, &'static TermInfo>,
-    ) -> Vec<(String, f32)>;
 }
 
 pub struct SearchTokenizeResult {
     pub should_expand: bool,
-    pub terms: Vec<(Option<String>, Vec<String>)>,
+    pub terms: Vec<SearchTokenizeTerm>,
+}
+
+pub struct SearchTokenizeTerm {
+    pub term: Option<String>,
+    pub term_inflections: Vec<String>,
+    pub original_term: String,
 }

packages/morsels_languages/morsels_lang_ascii/src/ascii.rs
@@ -1,19 +1,18 @@
 use std::borrow::Cow;
 #[cfg(feature = "indexer")]
 use std::collections::HashSet;
-use std::collections::BTreeMap;
 
+use morsels_common::dictionary::Dictionary;
 #[cfg(feature = "indexer")]
 use regex::Regex;
-use smartstring::alias::String as SmartString;
 
-use crate::ascii_folding_filter;
+use crate::{ascii_folding_filter, spelling};
 use crate::stop_words::get_stop_words;
 use crate::utils::{term_filter, split_terms};
 use morsels_common::MorselsLanguageConfig;
 #[cfg(feature = "indexer")]
 use morsels_common::tokenize::{IndexerTokenizer, TermIter};
-use morsels_common::tokenize::{TermInfo, SearchTokenizeResult, SearchTokenizer};
+use morsels_common::tokenize::{SearchTokenizeResult, SearchTokenizer, SearchTokenizeTerm};
 
 #[cfg(feature = "indexer")]
 lazy_static! {
@@ -103,7 +102,7 @@ impl IndexerTokenizer for Tokenizer {
 }
 
 impl SearchTokenizer for Tokenizer {
-    fn search_tokenize(&self, mut text: String) -> SearchTokenizeResult {
+    fn search_tokenize(&self, mut text: String, dict: &Dictionary) -> SearchTokenizeResult {
         text.make_ascii_lowercase();
 
         let should_expand = !text.ends_with(' ');
@@ -117,17 +116,42 @@ impl SearchTokenizer for Tokenizer {
 
                 let mut term_inflections = Vec::new();
 
-                let preprocessed = ascii_and_nonword_filter(&mut term_inflections, term_slice);
+                let mut preprocessed = ascii_and_nonword_filter(&mut term_inflections, term_slice);
 
                 if preprocessed.is_empty() {
                     return None;
                 }
 
+                let original_term = preprocessed.clone().into_owned();
+
+                // This comes before spelling correction,
+                // as ignore_stop_words removes from the index (won't be present in the dictionary)
                 if self.ignore_stop_words && self.is_stop_word(&preprocessed) {
-                    return Some((None, term_inflections));
+                    return Some(SearchTokenizeTerm {
+                        term: None,
+                        term_inflections,
+                        original_term,
+                    });
                 }
 
-                Some((Some(preprocessed.into_owned()), term_inflections))
+                if dict.get_term_info(&preprocessed).is_none() {
+                    if let Some(corrected_term) = spelling::get_best_corrected_term(dict, &preprocessed) {
+                        term_inflections.push(corrected_term.clone());
+                        preprocessed = Cow::Owned(corrected_term);
+                    } else {
+                        return Some(SearchTokenizeTerm {
+                            term: None,
+                            term_inflections,
+                            original_term,
+                        });
+                    }
+                }
+
+                Some(SearchTokenizeTerm {
+                    term: Some(preprocessed.into_owned()),
+                    term_inflections,
+                    original_term,
+                })
             })
             .collect();
 
@@ -140,26 +164,5 @@ impl SearchTokenizer for Tokenizer {
     #[inline(never)]
     fn is_stop_word(&self, term: &str) -> bool {
         self.stop_words.iter().any(|t| t == term)
-    }
-
-    fn use_default_fault_tolerance(&self) -> bool {
-        true
-    }
-
-    fn get_best_corrected_term(
-        &self,
-        _term: &str,
-        _dictionary: &BTreeMap<SmartString, &'static TermInfo>,
-    ) -> Option<String> {
-        None
-    }
-
-    fn get_prefix_terms(
-        &self,
-        _number_of_expanded_terms: usize,
-        _term: &str,
-        _dictionary: &BTreeMap<SmartString, &'static TermInfo>,
-    ) -> Vec<(String, f32)> {
-        Vec::new()
     }
 }

packages/morsels_languages/morsels_lang_chinese/src/chinese.rs
@@ -2,14 +2,12 @@
 use std::borrow::Cow;
 #[cfg(feature = "indexer")]
 use std::collections::HashSet;
-use std::collections::BTreeMap;
 
 use jieba_rs::Jieba;
-use smartstring::alias::String as SmartString;
 
 #[cfg(feature = "indexer")]
 use morsels_common::tokenize::{IndexerTokenizer, TermIter};
-use morsels_common::{tokenize::{TermInfo, SearchTokenizeResult, SearchTokenizer}, MorselsLanguageConfig};
+use morsels_common::{tokenize::{SearchTokenizeResult, SearchTokenizer, SearchTokenizeTerm}, MorselsLanguageConfig, dictionary::Dictionary};
 #[cfg(feature = "indexer")]
 use morsels_lang_ascii::ascii_folding_filter;
 use morsels_lang_ascii::ascii::ascii_and_nonword_filter;
@@ -99,7 +97,7 @@ impl IndexerTokenizer for Tokenizer {
 }
 
 impl SearchTokenizer for Tokenizer {
-    fn search_tokenize(&self, mut text: String) -> SearchTokenizeResult {
+    fn search_tokenize(&self, mut text: String, dict: &Dictionary) -> SearchTokenizeResult {
         text.make_ascii_lowercase();
 
         let should_expand = !text.ends_with(' ');
@@ -122,11 +120,22 @@ impl SearchTokenizer for Tokenizer {
                     return None;
                 }
 
-                if self.ignore_stop_words && self.is_stop_word(&filtered) {
-                    return Some((None, term_inflections));
+                let original_term = filtered.to_owned();
+
+                if self.ignore_stop_words && self.is_stop_word(&filtered)
+                    || dict.get_term_info(&filtered).is_none() {
+                    return Some(SearchTokenizeTerm {
+                        term: None,
+                        term_inflections,
+                        original_term,
+                    });
                 }
 
-                Some((Some(filtered), term_inflections))
+                Some(SearchTokenizeTerm {
+                    term: Some(filtered),
+                    term_inflections,
+                    original_term,
+                })
             })
             .collect();
 
@@ -139,26 +148,5 @@ impl SearchTokenizer for Tokenizer {
     #[inline(never)]
     fn is_stop_word(&self, term: &str) -> bool {
         self.stop_words.iter().any(|t| t == term)
-    }
-
-    fn use_default_fault_tolerance(&self) -> bool {
-        true
-    }
-
-    fn get_best_corrected_term(
-        &self,
-        _term: &str,
-        _dictionary: &BTreeMap<SmartString, &'static TermInfo>,
-    ) -> Option<String> {
-        None
-    }
-
-    fn get_prefix_terms(
-        &self,
-        _number_of_expanded_terms: usize,
-        _term: &str,
-        _dictionary: &BTreeMap<SmartString, &'static TermInfo>,
-    ) -> Vec<(String, f32)> {
-        Vec::new()
     }
 }

packages/morsels_languages/morsels_lang_latin/src/latin.rs
@@ -1,18 +1,16 @@
 use std::borrow::Cow;
 #[cfg(feature = "indexer")]
 use std::collections::HashSet;
-use std::collections::BTreeMap;
 
 use rust_stemmers::{Algorithm, Stemmer};
-use smartstring::alias::String as SmartString;
 
-use morsels_common::MorselsLanguageConfig;
+use morsels_common::{MorselsLanguageConfig, dictionary::Dictionary, tokenize::SearchTokenizeTerm};
 #[cfg(feature = "indexer")]
 use morsels_common::tokenize::{IndexerTokenizer, TermIter};
-use morsels_common::tokenize::{TermInfo, SearchTokenizeResult, SearchTokenizer};
+use morsels_common::tokenize::{SearchTokenizeResult, SearchTokenizer};
 #[cfg(feature = "indexer")]
 use morsels_lang_ascii::ascii_folding_filter;
-use morsels_lang_ascii::{ascii::ascii_and_nonword_filter, utils::split_terms};
+use morsels_lang_ascii::{ascii::ascii_and_nonword_filter, utils::split_terms, spelling};
 #[cfg(feature = "indexer")]
 use morsels_lang_ascii::ascii::SENTENCE_SPLITTER;
 use morsels_lang_ascii::stop_words::get_stop_words;
@@ -110,7 +108,7 @@ impl IndexerTokenizer for Tokenizer {
 }
 
 impl SearchTokenizer for Tokenizer {
-    fn search_tokenize(&self, mut text: String) -> SearchTokenizeResult {
+    fn search_tokenize(&self, mut text: String, dict: &Dictionary) -> SearchTokenizeResult {
         text.make_ascii_lowercase();
 
         let should_expand = !text.ends_with(' ');
@@ -125,7 +123,7 @@ impl SearchTokenizer for Tokenizer {
                 let mut term_inflections = Vec::new();
                 let preprocessed = ascii_and_nonword_filter(&mut term_inflections, term_slice);
 
-                let stemmed = if let Cow::Owned(v) = self.stemmer.stem(&preprocessed) {
+                let mut stemmed = if let Cow::Owned(v) = self.stemmer.stem(&preprocessed) {
                     term_inflections.push(v.clone());
                     Cow::Owned(v)
                 } else {
@@ -136,11 +134,36 @@ impl SearchTokenizer for Tokenizer {
                     return None;
                 }
 
+                let original_term = stemmed.clone().into_owned();
+
+                // This comes before spelling correction,
+                // as ignore_stop_words removes from the index (won't be present in the dictionary)
                 if self.ignore_stop_words && self.is_stop_word(&preprocessed) {
-                    return Some((None, term_inflections));
+                    return Some(SearchTokenizeTerm {
+                        term: None,
+                        term_inflections,
+                        original_term,
+                    });
                 }
 
-                Some((Some(stemmed.into_owned()), term_inflections))
+                if dict.get_term_info(&stemmed).is_none() {
+                    if let Some(corrected_term) = spelling::get_best_corrected_term(dict, &stemmed) {
+                        term_inflections.push(corrected_term.clone());
+                        stemmed = Cow::Owned(corrected_term);
+                    } else {
+                        return Some(SearchTokenizeTerm {
+                            term: None,
+                            term_inflections,
+                            original_term,
+                        });
+                    }
+                }
+
+                Some(SearchTokenizeTerm {
+                    term: Some(stemmed.into_owned()),
+                    term_inflections,
+                    original_term,
+                })
             })
             .collect();
 
@@ -153,26 +176,5 @@ impl SearchTokenizer for Tokenizer {
     #[inline(never)]
     fn is_stop_word(&self, term: &str) -> bool {
         self.stop_words.iter().any(|t| t == term)
-    }
-
-    fn use_default_fault_tolerance(&self) -> bool {
-        true
-    }
-
-    fn get_best_corrected_term(
-        &self,
-        _term: &str,
-        _dictionary: &BTreeMap<SmartString, &'static TermInfo>,
-    ) -> Option<String> {
-        None
-    }
-
-    fn get_prefix_terms(
-        &self,
-        _number_of_expanded_terms: usize,
-        _term: &str,
-        _dictionary: &BTreeMap<SmartString, &'static TermInfo>,
-    ) -> Vec<(String, f32)> {
-        Vec::new()
     }
 }

packages/morsels_search/src/searcher/query_parser.rs
@@ -1,4 +1,7 @@
-use morsels_common::tokenize::SearchTokenizer;
+use morsels_common::{
+    tokenize::{SearchTokenizer, SearchTokenizeTerm},
+    dictionary::Dictionary,
+};
 
 #[cfg_attr(test, derive(Debug, Eq, PartialEq))]
 pub enum QueryPartType {
@@ -230,25 +233,27 @@ fn handle_terminator(
     escape_indices: &[usize],
     query_parts: &mut Vec<QueryPart>,
     operator_stack: &mut Vec<Operator>,
+    dict: &Dictionary,
 ) {
     if i == j {
         return;
     }
 
-    let tokenize_result = tokenizer.search_tokenize(collect_slice(query_chars, i, j, escape_indices));
+    let tokenize_result = tokenizer.search_tokenize(collect_slice(query_chars, i, j, escape_indices), dict);
     if tokenize_result.terms.is_empty() {
         return;
     }
 
     let mut is_first = true;
-    for (term, term_inflections) in tokenize_result.terms {
+    for SearchTokenizeTerm {
+        term,
+        term_inflections,
+        original_term,
+    } in tokenize_result.terms {
         query_parts.push(QueryPart {
             should_expand: tokenize_result.should_expand,
-            terms: if let Some(term) = term {
-                Some(vec![term])
-            } else {
-                None
-            },
+            terms: term.map(|t| vec![t]),
+            original_terms: Some(vec![original_term]),
             terms_searched: Some(vec![term_inflections]),
             ..QueryPart::get_base(QueryPartType::Term)
         });
@@ -265,6 +270,7 @@ pub fn parse_query(
     tokenizer: &dyn SearchTokenizer,
     valid_fields: &Vec<&str>,
     with_positions: bool,
+    dict: &Dictionary,
 ) -> Vec<QueryPart> {
     let mut query_parts: Vec<QueryPart> = Vec::with_capacity(5);
 
@@ -289,12 +295,16 @@ pub fn parse_query(
                     let content = collect_slice(&query_chars, i, j, &escape_indices);
                     query_parse_state = QueryParseState::None;
 
-                    let tokenize_result = tokenizer.search_tokenize(content);
+                    let tokenize_result = tokenizer.search_tokenize(content, dict);
 
                     let mut terms = Vec::new();
                     let mut terms_searched = Vec::new();
 
-                    for (term, term_inflections) in tokenize_result.terms {
+                    for SearchTokenizeTerm {
+                        term,
+                        term_inflections,
+                        original_term: _,
+                    } in tokenize_result.terms {
                         if let Some(term) = term {
                             terms.push(term);
                         }
@@ -327,6 +337,7 @@ pub fn parse_query(
                         &escape_indices,
                         &mut query_parts,
                         &mut op_stack,
+                        dict,
                     );
 
                     i = j + 1;
@@ -384,6 +395,7 @@ pub fn parse_query(
                             &escape_indices,
                             &mut query_parts,
                             &mut op_stack,
+                            dict,
                         );
 
                         op_stack.push(Operator::Field(field_name));
@@ -410,6 +422,7 @@ pub fn parse_query(
                             &escape_indices,
                             &mut query_parts,
                             &mut op_stack,
+                            dict,
                         );
 
                         if query_parts.is_empty()
@@ -453,6 +466,7 @@ pub fn parse_query(
                         &escape_indices,
                         &mut query_parts,
                         &mut op_stack,
+                        dict,
                     );
 
                     op_stack.push(Operator::Not);
@@ -486,6 +500,7 @@ pub fn parse_query(
         &escape_indices,
         &mut query_parts,
         &mut op_stack,
+        dict,
     );
 
     query_parts
@@ -493,10 +508,13 @@ pub fn parse_query(
 
 #[cfg(test)]
 pub mod test {
-    use morsels_common::{MorselsLanguageConfig, MorselsLanguageConfigOpts};
+    use std::collections::BTreeMap;
+
+    use morsels_common::{MorselsLanguageConfig, MorselsLanguageConfigOpts, dictionary::Dictionary, tokenize::TermInfo};
     use pretty_assertions::assert_eq;
 
     use morsels_lang_ascii::ascii;
+    use smartstring::{SmartString, LazyCompact};
 
     use super::{QueryPart, QueryPartType};
 
@@ -536,6 +554,28 @@ pub mod test {
             self.field_name = Some(field_name.to_owned());
             self
         }
+    }
+
+    fn get_dictionary() -> Dictionary {
+        static TERM_INFO: TermInfo = TermInfo {
+            doc_freq: 1,
+            postings_file_name: 0,
+            postings_file_offset: 65535,
+        };
+
+        let mut term_infos: BTreeMap<SmartString<LazyCompact>, &'static TermInfo> = BTreeMap::default();
+
+        for term in vec![
+            "lorem", "ipsum", "for", "by", "and", "notipsum", "http", "localhost",
+            "8080", "title", "body", "not", "invalidfield",
+        ] {
+            term_infos.insert(
+                SmartString::from(term),
+                &TERM_INFO,
+            );
+        }
+
+        Dictionary { term_infos }
     }
 
     fn wrap_in_not(query_part: QueryPart) -> QueryPart {
@@ -589,7 +629,7 @@ pub mod test {
             is_stop_word_removed: false,
             should_expand: true,
             is_expanded: false,
-            original_terms: None,
+            original_terms: Some(vec![term.to_owned()]),
             terms: Some(vec![term.to_owned()]),
             terms_searched: Some(vec![vec![term.to_owned()]]),
             part_type: QueryPartType::Term,
@@ -627,7 +667,13 @@ pub mod test {
             options: MorselsLanguageConfigOpts::default(),
         });
 
-        super::parse_query(query.to_owned(), &tokenizer, &vec!["title", "body"], true)
+        super::parse_query(
+            query.to_owned(),
+            &tokenizer,
+            &vec!["title", "body"],
+            true,
+            &get_dictionary(),
+        )
     }
 
     pub fn parse_wo_pos(query: &str) -> Vec<QueryPart> {
@@ -636,7 +682,13 @@ pub mod test {
             options: MorselsLanguageConfigOpts::default(),
         });
 
-        super::parse_query(query.to_owned(), &tokenizer, &vec!["title", "body"], false)
+        super::parse_query(
+            query.to_owned(),
+            &tokenizer,
+            &vec!["title", "body"],
+            false,
+            &get_dictionary(),
+        )
     }
 
     // The tokenizer will remove stop words if they are not even indexed
@@ -651,7 +703,13 @@ pub mod test {
             },
         });
 
-        super::parse_query(query.to_owned(), &tokenizer, &vec!["title", "body"], true)
+        super::parse_query(
+            query.to_owned(),
+            &tokenizer,
+            &vec!["title", "body"],
+            true,
+            &get_dictionary(),
+        )
     }
 
     #[test]

packages/morsels_languages/morsels_lang_ascii/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod ascii_folding_filter;
 pub mod ascii;
+pub mod spelling;
 pub mod stop_words;
 pub mod utils;
 

packages/morsels_languages/morsels_lang_ascii/src/spelling.rs
@@ -1,0 +1,49 @@
+use morsels_common::dictionary::Dictionary;
+
+mod edit_distance;
+
+pub fn get_best_corrected_term(dict: &Dictionary, misspelled_term: &str) -> Option<std::string::String> {
+    let mut best_term = None;
+    let mut max_doc_freq = 0;
+
+    let base_term_char_count = misspelled_term.chars().count();
+    let mut min_edit_distance: usize = match base_term_char_count {
+        0..=3 => 1,
+        4..=7 => 2,
+        _ => 3,
+    };
+
+    let mut cache = [255_usize; 255];
+
+    for (term, term_info) in dict.term_infos.iter() {
+        if term.chars().count().abs_diff(base_term_char_count) > min_edit_distance {
+            continue;
+        }
+
+        if min_edit_distance == 1 && term_info.doc_freq < max_doc_freq {
+            continue;
+        }
+
+        let edit_distance = edit_distance::levenshtein(
+            term,
+            misspelled_term,
+            base_term_char_count,
+            &mut cache,
+        );
+        if edit_distance < min_edit_distance {
+            min_edit_distance = edit_distance;
+            max_doc_freq = term_info.doc_freq;
+            best_term = Some(term);
+        } else if edit_distance == min_edit_distance && term_info.doc_freq > max_doc_freq {
+            max_doc_freq = term_info.doc_freq;
+            best_term = Some(term);
+        }
+    }
+
+    if let Some(best_term) = best_term {
+        let normal_string = std::string::String::from(&best_term[..]);
+        Some(normal_string)
+    } else {
+        None
+    }
+}

commit f18b5553910fa852412786511e88d1031a4feaeb

Fix missing spelling correction info in QueryPart
Regression from c9392c651e497fb671b952ad165f7f07c8c6c6dc

packages/morsels_common/src/tokenize.rs
@@ -26,4 +26,5 @@ pub struct SearchTokenizeTerm {
     pub term_inflections: Vec<String>,
     pub original_term: String,
     pub suffix_wildcard: bool,
+    pub is_corrected: bool,
 }

packages/morsels_languages/morsels_lang_ascii/src/ascii.rs
@@ -149,6 +149,7 @@ impl SearchTokenizer for Tokenizer {
             }
 
             let original_term = preprocessed.clone().into_owned();
+            let mut is_corrected = false;
 
             // This comes before spelling correction,
             // as ignore_stop_words removes from the index (won't be present in the dictionary)
@@ -158,6 +159,7 @@ impl SearchTokenizer for Tokenizer {
                     term_inflections,
                     original_term,
                     suffix_wildcard,
+                    is_corrected,
                 });
                 continue;
             }
@@ -167,6 +169,7 @@ impl SearchTokenizer for Tokenizer {
                     None
                 } else if let Some(corrected_term) = spelling::get_best_corrected_term(dict, &preprocessed) {
                     term_inflections.push(corrected_term.clone());
+                    is_corrected = true;
                     Some(corrected_term)
                 } else {
                     None
@@ -180,6 +183,7 @@ impl SearchTokenizer for Tokenizer {
                 term_inflections,
                 original_term,
                 suffix_wildcard,
+                is_corrected,
             });
         }
 

packages/morsels_languages/morsels_lang_chinese/src/chinese.rs
@@ -131,6 +131,7 @@ impl SearchTokenizer for Tokenizer {
                         term_inflections,
                         original_term,
                         suffix_wildcard,
+                        is_corrected: false,
                     });
                 }
 
@@ -139,6 +140,7 @@ impl SearchTokenizer for Tokenizer {
                     term_inflections,
                     original_term,
                     suffix_wildcard,
+                    is_corrected: false,
                 })
             })
             .collect();

packages/morsels_languages/morsels_lang_latin/src/latin.rs
@@ -162,6 +162,7 @@ impl SearchTokenizer for Tokenizer {
             }
 
             let original_term = stemmed.clone().into_owned();
+            let mut is_corrected = false;
 
             // This comes before spelling correction,
             // as ignore_stop_words removes from the index (won't be present in the dictionary)
@@ -171,6 +172,7 @@ impl SearchTokenizer for Tokenizer {
                     term_inflections,
                     original_term,
                     suffix_wildcard,
+                    is_corrected,
                 });
                 continue;
             }
@@ -180,6 +182,7 @@ impl SearchTokenizer for Tokenizer {
                     None
                 } else if let Some(corrected_term) = spelling::get_best_corrected_term(dict, &stemmed) {
                     term_inflections.push(corrected_term.clone());
+                    is_corrected = true;
                     Some(corrected_term)
                 } else {
                     None
@@ -193,6 +196,7 @@ impl SearchTokenizer for Tokenizer {
                 term_inflections,
                 original_term,
                 suffix_wildcard,
+                is_corrected,
             });
         }
 

packages/morsels_search/src/searcher/query_parser.rs
@@ -297,10 +297,12 @@ fn handle_terminator(
         term_inflections,
         original_term,
         suffix_wildcard,
+        is_corrected,
     } in tokenize_result.terms {
         query_parts.push(QueryPart {
             auto_suffix_wildcard: tokenize_result.auto_suffix_wildcard,
             suffix_wildcard,
+            is_corrected,
             terms: term.map(|t| vec![t]),
             original_terms: Some(vec![original_term]),
             terms_searched: Some(vec![term_inflections]),
@@ -349,13 +351,16 @@ pub fn parse_query(
                     let mut terms = Vec::new();
                     let mut terms_searched = Vec::new();
 
+                    let mut is_corrected = false;
                     for SearchTokenizeTerm {
                         term,
                         term_inflections,
                         original_term: _,
                         // TODO unsupported for now
                         suffix_wildcard: _,
+                        is_corrected: is_term_corrected,
                     } in tokenize_result.terms {
+                        is_corrected = is_corrected || is_term_corrected;
                         if let Some(term) = term {
                             terms.push(term);
                         }
@@ -366,6 +371,7 @@ pub fn parse_query(
                     query_parts.push(QueryPart {
                         terms: Some(terms),
                         terms_searched: Some(terms_searched),
+                        is_corrected,
                         ..QueryPart::get_base(QueryPartType::Phrase)
                     });
                     handle_op(&mut query_parts, &mut op_stack);
@@ -601,6 +607,15 @@ pub mod test {
             }
         }
 
+        fn with_original_terms(mut self, original_terms: Vec<&str>) -> QueryPart {
+            if self.terms.is_some() {
+                self.original_terms = Some(original_terms.into_iter().map(|s| s.to_owned()).collect());
+                self
+            } else {
+                panic!("Tried to call with_searched_terms test function on query part with no terms");
+            }
+        }
+
         fn with_field(mut self, field_name: &str) -> QueryPart {
             self.field_name = Some(field_name.to_owned());
             self
@@ -612,6 +627,18 @@ pub mod test {
                 self
             } else {
                 panic!("Tried to call no_term test function on non-term query part");
+            }
+        }
+
+        fn with_corrected(mut self) -> QueryPart {
+            if (
+                matches!(self.part_type, QueryPartType::Term)
+                || matches!(self.part_type, QueryPartType::Phrase)
+            ) {
+                self.is_corrected = true;
+                self
+            } else {
+                panic!("Tried to call no_term test function on non-term/phrase query part");
             }
         }
     }
@@ -1058,6 +1085,29 @@ pub mod test {
                 get_lorem().with_field("title"),
             ]
         );
+    }
+
+    #[test]
+    fn spelling_correction_test() {
+        assert_eq!(parse("lore"), vec![
+            get_lorem()
+                .with_corrected()
+                .with_original_terms(vec!["lore"])
+                .with_searched_terms(vec![vec!["lore", "lorem"]]),
+        ]);
+        assert_eq!(parse("lore AND ipsum"), vec![wrap_in_and(vec![
+            get_lorem()
+                .with_corrected()
+                .with_original_terms(vec!["lore"])
+                .with_searched_terms(vec![vec!["lore", "lorem"]]),
+            get_ipsum(),
+        ])]);
+        assert_eq!(parse("\"lore ipsum\" AND ipsum"), vec![wrap_in_and(vec![
+            get_phrase(vec!["lorem", "ipsum"])
+                .with_corrected()
+                .with_searched_terms(vec![vec!["lore", "lorem"], vec!["ipsum"]]),
+            get_ipsum(),
+        ])]);
     }
 
     #[test]