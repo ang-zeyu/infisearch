02259f0ffd439f4d519088faba562dcea462cba5
Ze Yu <angzeyu@gmail.com>
2022-11-11 20:32:56 +0800

    Rename docs demo theme

--- docs/src/infi-theme.js
+++ docs/src/infi-theme.js
@@ -8,6 +8,6 @@
     // Add a new theme: https://github.com/rust-lang/mdBook/issues/605#issuecomment-362927102
     const theme_list = document.getElementById('theme-list');
     const theme = document.createElement('li');
-    theme.innerHTML = '<button class="theme" id="morsels-theme">Morsels</button>';
+    theme.innerHTML = '<button class="theme" id="infi-theme">Morsels</button>';
     theme_list.appendChild(theme);
 })()
